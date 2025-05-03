06a6be330b86a3e89aefaa59ad47d0301c5f0670
Dionatas Gabriel <seu@email.com>
2025-05-03 18:54:44 -0300

    Estrutura inicial da API

---
+++ Controllers/TarefasController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ListaTarefasApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TarefasController : ControllerBase
+    {
+        // GET: api/Tarefas
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "API de Lista de Tarefas funcionando!" });
+        }
+    }
+}
